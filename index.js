c83b766e1d1a44d44fbd9ed12314915be72f77ea
Sasa Milosevic <sasmilo20@gmail.com>
2021-01-21 00:08:47 +0100

    Some more gibberish

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const axios = require('axios');
+const { parse } = require('node-html-parser');
 
 (async () => {
   const page = await axios.get(
     'https://memegen-link-examples-upleveled.netlify.app/',
   );
-  console.log(page);
-})(); // Scrapes the content of the whole page
+  const pageData = page.data; // In pageData variable is the whole HTML page, but in form of a string
+  //console.log(pageData);
+
+  const dom = parse(pageData);
+  const img = dom.querySelector('.images'); // Should extract all src links that belong to the images
+  //const img = [];
+  //img = pageData.querySelector('img');
+  //console.log(dom);
+  //console.log(pageData);
+  //console.log(img);
+  console.log(img.getAttribute(src));
+})();
