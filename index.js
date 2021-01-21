9dcec4f62c46c9fc53b8cf935d7aa8539f7f8a36
Sasa Milosevic <sasmilo20@gmail.com>
2021-01-21 22:40:47 +0100

    Now we have an array of memes urls

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const axios = require('axios');
-const { parse } = require('node-html-parser');
 
 (async () => {
   const page = await axios.get(
     'https://memegen-link-examples-upleveled.netlify.app/',
   );
-  const pageData = page.data; // In pageData variable is the whole HTML page, but in form of a string
+  pageData = page.data; // In pageData variable is the whole HTML page, but in form of a string
   //console.log(pageData);
 
-  const dom = parse(pageData);
-  const img = dom.querySelector('.images'); // Should extract all src links that belong to the images
-  //const img = [];
-  //img = pageData.querySelector('img');
-  //console.log(dom);
-  //console.log(pageData);
-  //console.log(img);
-  console.log(img.getAttribute(src));
+  //console.log(pageData.includes('&')); // I've found out that string contained in pageData
+  //doesn't contain symbol '&'
+
+  const newPageDataString = pageData.replace(/src="/g, '&');
+  //Replace all 'src="' with exclusive sign '&' so we can split with '&'
+
+  //console.log(newPageDataString); // Just to prove it works
+
+  const arrayOfDataStrings = newPageDataString.split('&');
+  //console.log(arrayOfDataStrings);
+
+  //Now we need to extract strings that begin after '&' and end before '?' and put those strings in an array
+
+  const shortenArrayOfDataStrings = arrayOfDataStrings.map(function (
+    item,
+    index,
+  ) {
+    return item.slice(0, item.indexOf('?') + 1);
+  });
+  //Let's try to shorten the strings in this array, in a way that we will cut off all after '?'
+  // Now we got URLs of all meme pictures
+  //console.log(shortenArrayOfDataStrings); // Just to prove it works
 })();
