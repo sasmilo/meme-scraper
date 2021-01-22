ef4a1b6d9eb14b027f2e1d85cd7cb357c5d005dd
Sasa Milosevic <sasmilo20@gmail.com>
2021-01-22 15:11:56 +0100

    Add option to create folder if there isnt any

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const axios = require('axios');
+const https = require('https');
+const fs = require('fs');
+
+
+try {
+  if (!fs.existsSync('./memes')) {
+    fs.mkdirSync('./memes');
+  }
+} catch (err) {
+  console.error(err);
+}
+// Creating a new folder for saving the loaded images
 
 (async () => {
   const page = await axios.get(
@@ -7,11 +19,11 @@ const axios = require('axios');
   pageData = page.data; // In pageData variable is the whole HTML page, but in form of a string
   //console.log(pageData);
 
-  //console.log(pageData.includes('&')); // I've found out that string contained in pageData
+  //console.log(pageData.includes('&')); // That's false, so I've found out that string contained in pageData
   //doesn't contain symbol '&'
 
   const newPageDataString = pageData.replace(/src="/g, '&');
-  //Replace all 'src="' with exclusive sign '&' so we can split with '&'
+  //Replace all 'src="' string parts with exclusive sign '&' so we can split with '&' later
 
   //console.log(newPageDataString); // Just to prove it works
 
@@ -19,14 +31,36 @@ const axios = require('axios');
   //console.log(arrayOfDataStrings);
 
   //Now we need to extract strings that begin after '&' and end before '?' and put those strings in an array
-
+  //We observed the links at the string, and all of useful content stands between '&' and first '?' sign
+  //Let's try to shorten the strings in this array, in a way that we will cut off all after '?'
   const shortenArrayOfDataStrings = arrayOfDataStrings.map(function (
     item,
     index,
   ) {
     return item.slice(0, item.indexOf('?') + 1);
   });
-  //Let's try to shorten the strings in this array, in a way that we will cut off all after '?'
-  // Now we got URLs of all meme pictures
+
+  // Now we got URLs of all meme pictures in the string
   //console.log(shortenArrayOfDataStrings); // Just to prove it works
+
+  //Now we need to download the actual images to the 'memes' folder
+
+  for (const imgUrls of shortenArrayOfDataStrings) {
+    //console.log(imgUrls);
+
+  //Right here we turned URL strings into an actual URLs
+
+   for (let count = 0; count=10, count++) {
+  if (imgUrls.item.includes("images") {
+
+   const file = fs.createWriteStream("./memes");
+   const request = https.get(item, function(response) {
+    response.pipe(file);
+  }
+  });
+
+  }
+
+   ;)
+  }
 })();
