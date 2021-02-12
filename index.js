b36ac9be70bb29b5db76b19c17110392b3539eda
Sasa Milosevic <sasmilo20@gmail.com>
2021-02-12 16:53:23 +0100

    Fixed splitting by the string of characters

--- index.js
+++ index.js
@@ -18,20 +18,20 @@ try {
   const pageData = page.data; // In pageData variable is the whole HTML page, but in form of a string
   // console.log(pageData);
 
-  //console.log(pageData.includes('&')); // That's false, so I've found out that string contained in pageData
-  //doesn't contain symbol '&'
+  // console.log(pageData.includes('&')); // That's false, so I've found out that string contained in pageData
+  // doesn't contain symbol '&'
 
-  const newPageDataString = pageData.replace(/src="/g, '&');
-  //Replace all 'src="' string parts with exclusive sign '&' so we can split with '&' later
+  // const newPageDataString = pageData.replace(/src="/g, '&');
+  // Replace all 'src="' string parts with exclusive sign '&' so we can split with '&' later
 
-  //console.log(newPageDataString); // Just to prove it works
+  // console.log(newPageDataString); // Just to prove it works
 
-  const arrayOfDataStrings = newPageDataString.split('&');
-  //console.log(arrayOfDataStrings);
+  const arrayOfDataStrings = pageData.split(/src="/g);
+  // console.log(arrayOfDataStrings);
 
-  //Now we need to extract strings that begin after '&' and end before '?' and put those strings in an array
-  //We observed the links at the string, and all of useful content stands between '&' and first '?' sign
-  //Let's try to shorten the strings in this array, in a way that we will cut off all after '?'
+  // Now we need to extract strings that begin after '&' and end before '?' and put those strings in an array
+  // We observed the links at the string, and all of useful content stands between '&' and first '?' sign
+  // Let's try to shorten the strings in this array, in a way that we will cut off all after '?'
   const shortenArrayOfDataStrings = arrayOfDataStrings.map(function (
     item,
     index,
@@ -40,14 +40,14 @@ try {
   });
 
   // Now we got URLs of all meme pictures in the string
-  //console.log(shortenArrayOfDataStrings); // Just to prove it works
+  // console.log(shortenArrayOfDataStrings); // Just to prove it works
 
-  //Now we need to download the actual images to the 'memes' folder
+  // Now we need to download the actual images to the 'memes' folder
 
-  //for (const imgUrls of shortenArrayOfDataStrings) {
-  //console.log(typeof imgUrls);
+  // for (const imgUrls of shortenArrayOfDataStrings) {
+  // console.log(typeof imgUrls);
 
-  //Right here we have turned URL strings into an actual URLs
+  // Right here we have turned URL strings into an actual URLs
 
   for (let i = 0; i < 10; i++) {
     const url = shortenArrayOfDataStrings[i + 2];
@@ -55,8 +55,8 @@ try {
       const file = fs.createWriteStream(`./memes/image${i}.jpeg`);
       const request = https.get(url, function (response) {
         response.pipe(file);
-      }); //);});
+      });
     }
   }
-  //}
+  console.log('Success! Check in your root folder for the folder named Memes');
 })();
